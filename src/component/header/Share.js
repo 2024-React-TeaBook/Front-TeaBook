340c3ab320de45cdeac381390a6d1f05227c2017
sg-hwang-333 <hsg151515@naver.com>
2024-04-16 20:26:06 +0900

    [FEAT] Header UI 완성

---
+++ src/component/header/Share.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Share = () => {
+
+    return (
+        <div className={'share-container'}>
+            <img className={'share'} src={'img/icon-share/share.png'} alt={'share url'}/>
+            <img className={'share'} src={'img/icon-share/instagram.png'} alt={'share instagram'}/>
+            <img className={'share'} src={'img/icon-share/kakaotalk.png'} alt={'share kakaotalk'}/>
+            <img className={'share'} src={'img/icon-share/twitter.png'} alt={'share twitter'}/>
+        </div>
+    )
+}
+
+export default Share;
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,12 +1,14 @@
-import ReactDOM from "react-dom";
-import logo from './img/logo.png';
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const Header = function(props) {
+import Share from './Share'
+const Header = function (props) {
     return (
         <div>
-            <header>
-                <img src={logo} className='Header-logo'/>
+            <header style={{background: "url('img/background.png') no-repeat center", backgroundSize:"cover"}}>
+                <div className="figure"></div>
+                <div className="title-container">
+                    <div className="title">책과 차의 만남이 이루어지다, 다독</div>
+                    <div className="sub-title">A meeting of Books and Teas</div>
+                </div>
+                <Share/>
             </header>
         </div>
     )
