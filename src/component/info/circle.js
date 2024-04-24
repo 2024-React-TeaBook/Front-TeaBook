38c75f506a8c827f6b460f5f4f15c19eea6a1966
sg-hwang-333 <hsg151515@naver.com>
2024-04-24 12:14:21 +0900

    [FEAT] 텍스트 회전 애니메이션 완성

---
+++ src/component/info/circle.js
@@ -0,0 +1,28 @@
+const Circle = () => {
+    window.onload = () => {
+        const text = document.querySelector(".text");
+        const textContent = text.innerText.trim();
+        const radius = 300;
+        
+        text.innerHTML = textContent
+            .split("")
+            .map((char, i) => {
+                const angle = (360 / textContent.length) * i;
+                const x = radius * Math.cos((angle - 90) * (Math.PI / 180));
+                const y = radius * Math.sin((angle - 90) * (Math.PI / 180));
+                return `<span style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%) translate(${x}px, ${y}px) rotate(${angle}deg);">${char}</span>`;
+            })
+            .join("");
+    };
+
+    return (
+        <div class="circle">
+            <div class="logo"></div>
+            <div class="text">
+                Tea And Book And
+            </div>
+        </div>
+    )
+}
+
+export default Circle;
