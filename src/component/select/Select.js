7d6529dda2d274366fcabd5dc8a37ceed8127d62
w2208hd <w2208hd@gmail.com>
2024-04-25 12:08:43 +0900

    Genre select 구분

--- src/component/select/Select.js
+++ src/component/select/Select.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import './select.css'
+import GenreSelect from "./GenreSelect";
 
 const Select = function () {
     return (<div>
@@ -13,49 +14,9 @@ const Select = function () {
                 <div className="line-div" id="line-div-2"></div>
             </div>
 
-            <div className="select-genre-div-parent">
-                <div className="select-genre-div">
-                    <div className="genre-div">
-                        <div className="genre-div-1 genre-romance-1" style={{
-                            background: "url('img/select/genre-romance.png') no-repeat center",
-                            backgroundSize: "cover"
-                        }}></div>
-                        <div className="genre-div-2 genre-romance-2">
-                            <div className="genre-text">
-                                <h1 className="genre-text-1">로맨스</h1>
-                                <h3 className="genre-text-2">#사랑 #만남 #설렘</h3>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div className="genre-div">
-                        <div className="genre-div-1 genre-horror-1" style={{
-                            background: "url('img/select/genre-horror.png') no-repeat center",
-                            backgroundSize: "cover"
-                        }}></div>
-                        <div className="genre-div-2 genre-horror-2">
-                            <div className="genre-text">
-                                    <h1 className="genre-text-1">공포</h1>
-                                    <h3 className="genre-text-2">#귀신 #어둠 #악몽</h3>
-                                </div>
-                            </div>
-                    </div>
+            <GenreSelect />
 
-                    <div className="genre-div">
-                        <div className="genre-div-1 genre-sf-1" style={{
-                            background: "url('img/select/genre-sf.png') no-repeat center",
-                            backgroundSize: "cover"
-                        }}></div>
-                        <div className="genre-div-2 genre-sf-2">
-                            <div className="genre-text">
-                                    <h1 className="genre-text-1">SF</h1>
-                                    <h3 className="genre-text-2">#미래 #우주 #로봇</h3>
-                                </div>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>)
 }
