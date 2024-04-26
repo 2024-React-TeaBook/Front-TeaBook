386b27e703d718eca12649a0f0d05c3f043a1001
sg-hwang-333 <hsg151515@naver.com>
2024-04-26 13:08:25 +0900

    [FEAT] 장르 정보 json 파일로 빼기, react 코드 수정

--- src/component/select/GenreSelect.js
+++ src/component/select/GenreSelect.js
@@ -1,52 +1,25 @@
-import React, { useState } from "react";
-import './select.css'
+import './select.css';
+import genreJsonInfo from '../../json/genreJsonInfo';
 
-const GenreSelect = function () {
+const GenreSelect = function ({ genreName }) {
     return (
-        <div className="select-genre-div-parent">
-            <div className="select-genre-div">
-                <div className="genre-div">
-                    <div className="genre-div-1 genre-romance-1" style={{
-                        background: "url('img/select/genre-romance.png') no-repeat center",
-                        backgroundSize: "cover"
-                    }}></div>
-                    <div className="genre-div-2 genre-romance-2">
-                        <div className="genre-text">
-                            <h1 className="genre-text-1">로맨스</h1>
-                            <h3 className="genre-text-2">#사랑 #만남 #설렘</h3>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="genre-div">
-                    <div className="genre-div-1 genre-horror-1" style={{
-                        background: "url('img/select/genre-horror.png') no-repeat center",
-                        backgroundSize: "cover"
-                    }}></div>
-                    <div className="genre-div-2 genre-horror-2">
-                        <div className="genre-text">
-                            <h1 className="genre-text-1">공포</h1>
-                            <h3 className="genre-text-2">#귀신 #어둠 #악몽</h3>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="genre-div">
-                    <div className="genre-div-1 genre-sf-1" style={{
-                        background: "url('img/select/genre-sf.png') no-repeat center",
-                        backgroundSize: "cover"
-                    }}></div>
-                    <div className="genre-div-2 genre-sf-2">
-                        <div className="genre-text">
-                            <h1 className="genre-text-1">SF</h1>
-                            <h3 className="genre-text-2">#미래 #우주 #로봇</h3>
-                        </div>
-
-                    </div>
+        <div className="genre-div">
+            <div className="genre-div-1" style={{
+                background: `url('img/select/genre-${genreName}.png') no-repeat center`,
+                backgroundSize: "cover"
+            }}></div>
+            <div className="genre-div-2">
+                <div className="genre-text">
+                    <h1 className="genre-text-1">{genreJsonInfo[genreName].title}</h1>
+                    <h3 className="genre-text-2">{
+                        genreJsonInfo[genreName].hashtag.map((e) => {
+                            return `#${e} `;
+                        })
+                    }</h3>
                 </div>
             </div>
         </div>
     )
 }
 
-export default GenreSelect
+export default GenreSelect;
---
+++ src/json/genreJsonInfo.js
@@ -0,0 +1,22 @@
+const genreJsonInfo = {
+    romance: {
+        title: "로맨스",
+        hashtag: ["사랑", "만남", "설렘"],
+        content: ["로맨스물은 사랑과 감정을 중심으로 한 이야기를 다루는 장르입니다.",
+                    "주로 로맨틱한 상상과 감성적인 책을 즐기는 분들께 추천합니다.",
+                    "휴식이 필요한 때나 사랑 이야기에 빠져들고 싶을 때 좋은 선택이며",
+                    "아름다운 메시지나 해피엔딩을 통해 긍정적인 영향을 받을 수 있습니다."]
+    },
+    horror: {
+        title: "공포",
+        hashtag: ["귀신", "어둠", "악몽"],
+        content: []
+    },
+    sf: {
+        title: "SF",
+        hashtag: ["미래", "우주", "로봇"],
+        content: []
+    }
+}
+
+export default genreJsonInfo;
