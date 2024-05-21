1ac6ed8100cfd12e443c6c16f04e962bffcdc1c3
sg-hwang-333 <hsg151515@naver.com>
2024-05-21 19:55:23 +0900

    [FEAT] [STYLE] tea title 수정, 장르설명 폰트 크기 변경

--- src/component/main/teaBook/tea/tea.js
+++ src/component/main/teaBook/tea/tea.js
@@ -1,8 +1,17 @@
 import './tea.css';
 
-const Tea = () => {
+const Tea = ({themeStyle}) => {
     return (
-        <div className="tea-container">
+        <div className="teaBook-container tea-container">
+            <div className='teaBook-title-container tea-title'>
+                <div>This kind of tea suits the book</div>
+                <div>이런 차와 어울려요</div>
+            </div>
+            <div className='tea'>
+                {
+                    
+                }
+            </div>
 
         </div>
     )
